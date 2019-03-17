c2c00b19a598126e5b1e27b66972e3a0d615c6d5
Arnar Gauti Ingason <arnarg@fastmail.com>
2019-03-17 12:51:39 +0000

    Adding support for Meson GXL P281 (Mostly copy from P212)

---
+++ include/configs/p281.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration for Tanix TX3-Mini (Amlogic P281)
+ *
+ * Copyright (C) 2018 Arnar Gauti Ingason
+ * Author: Arnar Gauti Ingason <arnarg@fastmail.com>
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+/* Serial setup */
+
+#define MESON_FDTFILE_SETTING "fdtfile=amlogic/meson-gxl-s905w-p281.dtb\0"
+
+#include <configs/meson-gx-common.h>
+
+#endif /* __CONFIG_H */
